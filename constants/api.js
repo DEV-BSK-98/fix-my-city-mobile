9871dabe244136c71af955c3a7e43648f2e243d1
DEV-BSK-98 <bskdev98@gmail.com>
2025-09-18 21:55:03 +0200

    updated

--- constants/api.js
+++ constants/api.js
@@ -1,2 +1,8 @@
-export const API_URL = 'http://10.20.24.100:3001/api'
+export const API_URL = 'http://192.9.200.73/api'
 // export const API_URL = 'http://10.152.5.22:3001/api'
+
+// step 1 type ipconfig
+// step 2 copy the IPv4 Address
+// step 3 paste it in the api.js file in the constants folder
+
+// and we are done
